18a6db12f53fd0778a49a19344c1140ba74397e1
issler78 <132786398+Issler78@users.noreply.github.com>
2026-02-13 21:27:44 -0300

    refactor: move generate response to controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,36 @@
+import { UserHelperService } from '@/helpers/user/userHelper.service';
 import { User } from '@/user/decorators/user.decorator';
 import { LoginUserDTO } from '@/user/DTOs/loginUser.dto';
 import { RegisterUserDTO } from '@/user/DTOs/registerUser.dto';
 import { AuthGuard } from '@/user/guards/auth.guard';
-import { UserRequest } from '@/user/interfaces/userRequest.interface';
 import { IUserResponse } from '@/user/interfaces/userResponse.interface';
 import { UserEntity } from '@/user/user.entity';
 import { UserService } from '@/user/user.service';
-import { Body, Controller, Get, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService ) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly userHelper: UserHelperService,
+  ) {}
 
   @Post('/register')
   @UsePipes(new ValidationPipe())
-  async create(@Body('user') registerDTO: RegisterUserDTO): Promise<IUserResponse> {
+  async create(
+    @Body('user') registerDTO: RegisterUserDTO,
+  ): Promise<IUserResponse> {
     const newUser = await this.userService.create(registerDTO);
 
-    return this.userService.generateResponse(newUser);
+    return this.generateResponse(newUser);
   }
 
   @Post('/login')
@@ -25,14 +38,26 @@ export class UserController {
   async login(@Body('user') loginDTO: LoginUserDTO): Promise<IUserResponse> {
     const user = await this.userService.login(loginDTO);
 
-    return this.userService.generateResponse(user);
+    return this.generateResponse(user);
   }
 
   @Get('/me')
   @UseGuards(AuthGuard)
   async getCurrent(@User('sub') userId: string) {
-    const user = await this.userService.findById(userId)
+    const user = await this.userService.findById(userId);
 
-    return this.userService.generateResponse(user);
+    return this.generateResponse(user);
+  }
+
+  generateResponse(user: UserEntity): IUserResponse {
+    return {
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        role: this.userHelper.getRolesByString(user.role),
+      },
+      token: this.userService.generateToken(user),
+    };
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,21 +1,22 @@
 import { RegisterUserDTO } from '@/user/DTOs/registerUser.dto';
-import { IUserResponse } from '@/user/interfaces/userResponse.interface';
 import { UserEntity } from '@/user/user.entity';
-import { HttpException, HttpStatus, Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { sign } from 'jsonwebtoken'; 
+import { sign } from 'jsonwebtoken';
 import { LoginUserDTO } from '@/user/DTOs/loginUser.dto';
 import { compare } from 'bcrypt';
-import { UserHelperService } from '@/helpers/user/userHelper.service';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
-
-    private readonly userHelper: UserHelperService
   ) {}
 
   async create(registerDTO: RegisterUserDTO): Promise<UserEntity> {
@@ -25,26 +26,23 @@ export class UserService {
 
     // try save the new user
     try {
-
       return await this.userRepository.save(newUser);
-
     } catch (error: any) {
-
       // if error code of postgres is an unique error (23505)
       if (error.code === '23505') {
-
         // map with key = field unique and value = error message
         const errorsMap = {
           email: 'This email already exists',
           phone: 'This phone already exists',
         };
 
-        const field = Object.keys(errorsMap).find(key => String(error.detail ?? '').includes(key)); // try find key on details in error
+        const field = Object.keys(errorsMap).find((key) =>
+          String(error.detail ?? '').includes(key),
+        ); // try find key on details in error
         throw new HttpException(
-          field ? errorsMap[field] : 'Unique field already exists', 
-          HttpStatus.CONFLICT
+          field ? errorsMap[field] : 'Unique field already exists',
+          HttpStatus.CONFLICT,
         );
-
       }
 
       throw new InternalServerErrorException();
@@ -53,13 +51,19 @@ export class UserService {
 
   async login(loginDTO: LoginUserDTO): Promise<UserEntity> {
     const user = await this.findByEmail(loginDTO.email); // try find user by email
-    if(!user) {
-      throw new HttpException('Invalid email or password', HttpStatus.UNAUTHORIZED)
+    if (!user) {
+      throw new HttpException(
+        'Invalid email or password',
+        HttpStatus.UNAUTHORIZED,
+      );
     }
 
     const matchPassword = await compare(loginDTO.password, user.password); // verify if same password in DB
-    if(!matchPassword) {
-      throw new HttpException('Invalid email or password', HttpStatus.UNAUTHORIZED)
+    if (!matchPassword) {
+      throw new HttpException(
+        'Invalid email or password',
+        HttpStatus.UNAUTHORIZED,
+      );
     }
 
     return user;
@@ -68,19 +72,19 @@ export class UserService {
   async findByEmail(email: string): Promise<UserEntity | null> {
     return await this.userRepository.findOne({
       where: {
-        email
-      }
+        email,
+      },
     });
   }
 
   async findById(id: string): Promise<UserEntity> {
     const user = await this.userRepository.findOne({
       where: {
-        id
-      }
+        id,
+      },
     });
 
-    if(!user) {
+    if (!user) {
       throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
 
@@ -91,21 +95,9 @@ export class UserService {
     return sign(
       {
         sub: user.id,
-        role: user.role
+        role: user.role,
       },
-      process.env.SECRET_KEY
-    )
-  }
-
-  generateResponse(user: UserEntity): IUserResponse {
-    return {
-      user: {
-        name: user.name,
-        email: user.email,
-        phone: user.phone,
-        role: this.userHelper.getRolesByString(user.role)
-      },
-      token: this.generateToken(user)
-    };
+      process.env.SECRET_KEY,
+    );
   }
 }
